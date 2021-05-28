29c4d27d9416b22c92dd9676db2bb35fe1e0d680
Suapkorn <supakorn_kantawong@cmu.ac.th>
2021-05-28 17:13:57 +0700

    Web_Java_01-#5
    Web_Java_01-#5

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-document.getElementById('ok').addEventListener('click', function(e)
+function showCoords(e)
 {
-    alert('ok is clicked')
+    console.log("event is fired on "+e.currentTarget.id+" X="+e.x+" Y="+e.y+
+    " offsetX="+e.offsetX+" offsetY="+e.offsetY+" clientX="+e.clientX+" clienty="+e.clientY+
+    " screenX="+e.screenX+" screenY="+e.screenY);
+}
+function clearCoor()
+{
+    console.log()
+}
+document.getElementById('ok').addEventListener('click', function()
+{
+    alert('OK')
+}
+)
+document.getElementById('cancel').addEventListener('click', function()
+{
+    alert('cancel')
+}
+)
+document.getElementById('no').addEventListener('click', function()
+{
+    alert('No')
 }
 )
